48d0e10656e7f1b3733e95185fa4be22dbbee9a7
Srujan Gurram <srujangs8@gmail.com>
2025-06-09 12:39:18 +0530

    Remove unused Telegram env variables

--- src/env.ts
+++ src/env.ts
@@ -12,8 +12,6 @@ export const envSchema = z.object({
 	DEBUG: z.stringbool().default(false),
 	GOOGLE_API_KEY: z.string(),
 	TELEGRAM_CHAT_ID: z.string(),
-	TELEGRAM_SERVER_KEY: z.string(),
-	TELEGRAM_PROFILE_ID: z.string(),
 	TELEGRAM_BOT_TOKEN: z.string(),
 	WALLET_PRIVATE_KEY: z.string(),
 	PATH: z.string(),
